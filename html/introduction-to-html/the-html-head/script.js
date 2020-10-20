1ffca2e906551e5712a12192e43bc2936fc63541
coolwahoo <coolwahoo@yahoo.fr>
2020-10-20 08:21:46 +0200

    Update script.js

--- html/introduction-to-html/the-html-head/script.js
+++ html/introduction-to-html/the-html-head/script.js
@@ -2,20 +2,20 @@ const list = document.createElement('ul');
 const info = document.createElement('p');
 const html = document.querySelector('html');
 
-info.textContent = 'Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
+info.textContent = "Voici une liste dynamique. Cliquez n'importe où en dehors de la liste pour ajouter un nouvel élément de liste. Cliquez sur un élément de liste existant pour changer son texte.";
 
 document.body.appendChild(info);
 document.body.appendChild(list);
 
 html.onclick = function() {
   const listItem = document.createElement('li');
-  const listContent = prompt('What content do you want the list item to have?');
+  const listContent = prompt("Quel contenu voulez-vous que l'élément de liste possède?");
   listItem.textContent = listContent;
   list.appendChild(listItem);
 
   listItem.onclick = function(e) {
     e.stopPropagation();
-    const listContent = prompt('Enter new content for your list item');
+    const listContent = prompt('Saisissez un nouveau contenu pour votre élément de liste');
     this.textContent = listContent;
   }
 }
